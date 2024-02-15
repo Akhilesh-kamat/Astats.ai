0a9c93a818c71e7d0ef197fcacb32fe0ddd7ae38
akhilesh <akhileshkamat1999dpt@gmail.com>
2024-02-16 03:19:02 +0530

    made changes in blue section

--- scripts/main.js
+++ scripts/main.js
@@ -5,12 +5,12 @@ const mobileNavWrapper = document.querySelector("#mobile-nav-wrapper");
 // Handling mobile menu
 
 
-const handleMobileMenu = () =>{
-    mobileNavWrapper.classList.toggle("active");
+const handleMobileMenu = () => {
+  mobileNavWrapper.classList.toggle("active");
 
 }
 
-menuBtn.addEventListener("click", handleMobileMenu )
+menuBtn.addEventListener("click", handleMobileMenu)
 
 // making header animation 
 
@@ -21,7 +21,7 @@ var prevScrollPos = window.document.pageYOffset;
 // Function to handle scroll event
 function handleScroll() {
   var currentScrollPos = window.pageYOffset;
-  
+
   if (prevScrollPos > currentScrollPos) {
     header.classList.add("sticky");
   } else {
@@ -34,5 +34,22 @@ function handleScroll() {
 // Add scroll event listener
 window.addEventListener("scroll", handleScroll);
 
-    // Add scroll event listener
-    window.addEventListener("scroll", handleScroll);
+// Add scroll event listener
+window.addEventListener("scroll", handleScroll);
+
+
+// circle active function
+
+var spans = document.querySelectorAll('.circle-wrap span');
+
+// Add click event listener to each span
+spans.forEach(span => {
+  span.addEventListener("click", () => {
+
+    spans.forEach(e => {
+      e.classList.remove("active");
+    })
+    span.classList.add("active");
+  })
+})
+
