4e80e698a01e7a9c6c7f24acbfc14e2d79b636e0
akhilesh <akhileshkamat1999dpt@gmail.com>
2024-03-02 05:53:48 +0530

    hero section

--- scripts/main.js
+++ scripts/main.js
@@ -12,30 +12,6 @@ const handleMobileMenu = () => {
 
 menuBtn.addEventListener("click", handleMobileMenu)
 
-// making header animation 
-
-var header = document.querySelector(".desktop-nav-wrapper");
-
-var prevScrollPos = window.document.pageYOffset;
-
-// Function to handle scroll event
-function handleScroll() {
-  var currentScrollPos = window.pageYOffset;
-
-  if (prevScrollPos > currentScrollPos) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-
-  prevScrollPos = currentScrollPos;
-}
-
-// Add scroll event listener
-window.addEventListener("scroll", handleScroll);
-
-// Add scroll event listener
-window.addEventListener("scroll", handleScroll);
 
 
 // circle active function 
